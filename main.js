26112de46bc5ebaff3426b7e421fbecfe00b78b8
eliyahubelay0@gmail.com <eliyahubelay0@gmail.com>
2022-10-02 23:52:36 +0300

    making a progress in DOM manipulation

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-const body = document.body;
+let word = document.getElementsByClassName("user_feedback");
+let btn = document.getElementById("btn");
+let containerAllContent = document.getElementsByClassName("container_all_content");
+let containerTYMessage = document.getElementsByClassName("container_thank_you_message");
+btn.onclick = function UserChoice(){
+    console.log(containerAllContent.length);
+    containerAllContent.style.display = "none";
+    console.log("hello world");
+}
+// word.onclick(go);
 
-body.append("hello world");
+
+// function helloWorldApeare(){
+//     body.append("hello world");
+// }
+// body.onclick(helloWorldApeare());
+
+// container_all_content
+// container_thank_you_message
